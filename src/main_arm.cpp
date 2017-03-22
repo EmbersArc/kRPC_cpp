f9a8cad8748d945ec30fc4337f3a4baa2f464b5f
EmbersArc <s-niederberger@hotmail.com>
2017-03-22 21:34:43 +0100

    It worked I swear

--- src/main_arm.cpp
+++ src/main_arm.cpp
@@ -38,74 +38,91 @@ double jointLength = 2.5; //m
 
 // transforms
 Vector3d JS; 	//Joint space coordinates
-Vector3d dJS; 	//Joint space coordinates
+Vector3d dJS; 	//Joint space adjustment
 
 Matrix3d J; 	//Jacobian
 
 Vector3d t;		//target OS coordinates
 Vector3d s;		//current OS coordinates
 Vector3d e;		//error = target - current
-double alpha;	//small number or something
+double alpha;	//adjustment scalar
 Vector3d JJte;	//just some intermediate step
 
 
 krpc::services::InfernalRobotics::ServoGroup group = ir.servo_group_with_name(vessel, "servos");
-
 krpc::services::InfernalRobotics::Servo servo1 = group.servo_with_name("servo1");
 krpc::services::InfernalRobotics::Servo servo2 = group.servo_with_name("servo2");
 krpc::services::InfernalRobotics::Servo servo3 = group.servo_with_name("servo3");
+
 krpc::services::SpaceCenter::ReferenceFrame ref_frame_surf = vessel.surface_reference_frame();
 
 int main() {
 
-	krpc::services::SpaceCenter::Part EE = vessel.parts().with_tag("Base")[0]; //the end effector
+	krpc::services::SpaceCenter::Part EE = vessel.parts().with_tag("EE")[0]; //the end effector
+	krpc::services::SpaceCenter::Part Base = vessel.parts().with_tag("Base")[0]; //the base joint
+	krpc::services::SpaceCenter::Part Target = vessel.parts().with_tag("Target")[0]; //the target object
 
-	krpc::services::SpaceCenter::Part Base = vessel.parts().with_tag("EE")[0]; //the base joint
 
-
-	std::tuple<double,double,double> EEPosition = vectorSubtract(Base.position(ref_frame_surf),EE.position(ref_frame_surf)); //position relative to base
-
-	std::cout << get<0>(EEPosition) << std::endl;
-	std::cout << get<1>(EEPosition) << std::endl;
-	std::cout << get<2>(EEPosition) << std::endl;
+	std::tuple<double,double,double> TargetPosition;
+	TargetPosition = vectorSubtract(Target.center_of_mass(ref_frame_surf),Base.center_of_mass(ref_frame_surf)); //position relative to base
 
 	dJS << 1,1,1;
-	t << 2,2,5;
-	s << get<0>(EEPosition),
-	 	get<1>(EEPosition),
-		get<2>(EEPosition);
+	t << -get<1>(TargetPosition),
+		get<2>(TargetPosition),
+		get<0>(TargetPosition);
+
+	e << 1,1,1;
 
 	JS << servo1.position(),servo2.position(),servo3.position();
+
+		std::cout << JS << std::endl << std::endl;
+
+
 	JS = JS * PI / 180;
 
-	while(dJS.norm() > 0.01){
 
-	//std::cout << JS << std::endl << std::endl;
 
-	J << jointLength*(-cos(JS(2))*sin(JS(0))*sin(JS(1))-cos(JS(1))*sin(JS(0))*sin(JS(2))), jointLength*(cos(JS(0))*cos(JS(1))*cos(JS(2))-cos(JS(0))*sin(JS(1))*sin(JS(2))), jointLength*(cos(JS(0))*cos(JS(1))*cos(JS(2))-cos(JS(0))*sin(JS(1))*sin(JS(2))),
-		jointLength*(cos(JS(0))*cos(JS(2))*sin(JS(1))-cos(JS(0))*cos(JS(1))*sin(JS(2))), jointLength*(cos(JS(1))*cos(JS(2))*sin(JS(0))-sin(JS(0))*sin(JS(1))*sin(JS(2))), jointLength*(cos(JS(1))*cos(JS(2))*sin(JS(0))-sin(JS(0))*sin(JS(1))*sin(JS(2))),
-		0.0, jointLength*(-cos(JS(2))*sin(JS(1))-cos(JS(1))*sin(JS(2))), jointLength*(-cos(JS(2))*sin(JS(1))-cos(JS(1))*sin(JS(2)));
-	//std::cout << J.format(CleanFmt) << std::endl;
+	// while(e.norm() > 0.01){
 
-	e = t - s; //compute error
+		// EE Position Model
+		s <<	
+		cos(JS(0))*(jointLength*cos(JS(2))*sin(JS(1))+jointLength*cos(JS(1))*sin(JS(2))),
+		sin(JS(0))*(jointLength*cos(JS(2))*sin(JS(1))+jointLength*cos(JS(1))*sin(JS(2))),
+		2*jointLength+jointLength*cos(JS(1))*cos(JS(2))-jointLength*sin(JS(1))*sin(JS(2));
+		std::cout << s << std::endl << std::endl;
 
-	JJte = J*J.transpose()*e;
 
-	alpha = e.dot(JJte) / JJte.dot(JJte);
 
-	dJS = alpha*J.transpose()*e; //iterative adjustment to joint space
+		// JACOBI
+		J.col(0)	<<	jointLength*(-cos(JS(2))*sin(JS(0))*sin(JS(1))-cos(JS(1))*sin(JS(0))*sin(JS(2))),
+						jointLength*(cos(JS(0))*cos(JS(2))*sin(JS(1))+cos(JS(0))*cos(JS(1))*sin(JS(2))),
+						0;
 
-	JS += dJS;
+		J.col(1)	<<	jointLength*(cos(JS(0))*cos(JS(1))*cos(JS(2))-cos(JS(0))*sin(JS(1))*sin(JS(2))),
+						jointLength*(cos(JS(1))*cos(JS(2))*sin(JS(0))-sin(JS(0))*sin(JS(1))*sin(JS(2))),
+						jointLength*(-cos(JS(2))*sin(JS(1))-cos(JS(1))*sin(JS(2)));
 
-	}
+		J.col(2)	<<	jointLength*(cos(JS(0))*cos(JS(1))*cos(JS(2))-cos(JS(0))*sin(JS(1))*sin(JS(2))),
+						jointLength*(cos(JS(1))*cos(JS(2))*sin(JS(0))-sin(JS(0))*sin(JS(1))*sin(JS(2))),
+						jointLength*(-cos(JS(2))*sin(JS(1))-cos(JS(1))*sin(JS(2)));
 
-	JS = JS * 180/PI;
+		e = t - s; //compute error
+		
+		std::cout << e.norm() << std::endl << std::endl;
+		JJte = J*J.transpose()*e;
+		alpha = e.dot(JJte) / JJte.dot(JJte);
+		dJS = alpha*J.transpose()*e; //iterative adjustment to joint space
+		JS += dJS;
+		
+	// }
 
-	std::cout << JS << std::endl;
+	JS = JS * 180 / PI;
+	// servo1.move_to(JS(0),5);
+	// servo2.move_to(JS(1),5);
+	// servo3.move_to(JS(2),5);
 
-	servo1.move_to(JS(0),5);
-	servo2.move_to(JS(1),5);
-	servo3.move_to(JS(2),5);
+	std::cout << "----DONE----" << std::endl;
+
 
 	while (true){
 		
@@ -114,21 +131,3 @@ int main() {
 
 }
 
-	
-// Matrix4d t01, t12, t23;
-
-// //	transform matrices
-// 	t01 << 	cos(JS(0)), 	-sin(JS(0)), 	0, 					0,
-// 			sin(JS(0)), 	cos(JS(0)),  	0, 					0,
-// 			0,		 		0,		   		1, 					jointLength,
-// 			0,		 		0,		   		0,					1;
-	
-// 	t12 << 	cos(JS(1)),		0,				sin(JS(1)),			0,
-// 			0,		 		1,  			0, 					0,
-// 			-sin(JS(1)),	0,  			cos(JS(1)), 		jointLength,
-// 			0,		 		0,				0, 					1;
-
-// 	t23 << 	cos(JS(2)),		0,				sin(JS(2)),			0,
-// 			0,		 		1,  			0, 					0,
-// 			-sin(JS(2)),	0,  			cos(JS(2)),			0,
-// 			0,		 		0,				0, 					1;
